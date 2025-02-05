792fc397e2dcad7ca676a4496f98902bc37c761f
temarusanov <artem.rusanov.02@gmail.com>
2025-02-05 12:41:54 +0700

    feat(health-checks): add k8s support

--- packages/health-checks/src/lib/controllers/health-checks.controller.ts
+++ packages/health-checks/src/lib/controllers/health-checks.controller.ts
@@ -1,15 +1,13 @@
 import { DiscoveryService } from '@golevelup/nestjs-discovery'
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Res } from '@nestjs/common'
 import { ExternalContextCreator } from '@nestjs/core'
+import { Response } from 'express'
 
 import {
     HEALTH_INDICATOR_ARGS_METADATA,
     HEALTH_INDICATOR_METADATA,
 } from '../constants/health-checks.constants'
-import {
-    HealthCheckResult,
-    HealthIndicatorResult,
-} from '../interfaces/health-checks.interfaces'
+import { HealthIndicatorResult } from '../interfaces/health-checks.interfaces'
 
 @Controller()
 export class HealthChecksController {
@@ -20,8 +18,15 @@ export class HealthChecksController {
         private readonly externalContextCreator: ExternalContextCreator,
     ) {}
 
-    @Get('/health')
-    async check(): Promise<HealthCheckResult> {
+    @Get('/health/live')
+    async checkLive() {
+        return {
+            healthy: true,
+        }
+    }
+
+    @Get('/health/ready')
+    async checkReady(@Res() res: Response) {
         const timestamp = Date.now()
         const uptime = Math.floor((timestamp - this._appStartedAt) / 1000)
 
@@ -60,13 +65,16 @@ export class HealthChecksController {
             (s) => s.status === 'up',
         ).length
         const ratio = +(successfulServices / services.length).toFixed(2)
+        const healthy = successfulServices === services.length
 
-        return {
-            healthy: successfulServices === services.length,
+        const statusCode = healthy ? 200 : 503
+
+        res.status(statusCode).json({
+            healthy,
             ratio,
             uptime,
             timestamp,
             services,
-        }
+        })
     }
 }
