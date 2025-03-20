46ec447e5e654e04d49a1bed2d77b53898dc262d
temarusanov <artem.rusanov.02@gmail.com>
2025-03-20 21:40:45 +0700

    fix: add exports to barrel file

--- packages/features/src/index.ts
+++ packages/features/src/index.ts
@@ -1,4 +1,9 @@
+export * from './lib/configs/feature-module.config'
 export * from './lib/decorators/feature.decorator'
 export * from './lib/features.module'
+export {
+    IFeatureBus,
+    IFeatureHandler,
+} from './lib/interfaces/features.interfaces'
 export * from './lib/services/bus.service'
 export * from './lib/services/explorer.service'
